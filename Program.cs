d139e2774ab034ddc4c3c8fb6eaa70e02088a9be
staslif <149203375+staslif@users.noreply.github.com>
2024-01-20 09:06:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Ввод размеров матрицы
+Console.Write("Введите количество строк матрицы: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов матрицы: ");
+int cols = int.Parse(Console.ReadLine());
+
+// Создание и заполнение двумерной матрицы
+int[,] matrix = new int[rows, cols];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write($"Введите элемент [{i},{j}]: ");
+        matrix[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+int sum = 0;
+// Поиск и суммирование модулей отрицательных нечетных элементов
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        if (matrix[i, j] < 0 && matrix[i, j] % 2 != 0)
+        {
+            sum += Math.Abs(matrix[i, j]);
+        }
+    }
+}
+
+Console.WriteLine($"Сумма модулей отрицательных нечетных элементов: {sum}");
+
+Console.ReadLine();
